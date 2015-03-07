1e1ad79cca886938585ff6192a91b1d64335e154
Timothee Bureaugodart <bureau_t@epita.fr>
2015-03-07 16:23:57 +0100

    Détecte couleur >200 rouge, set pixel à 255(rouge)

--- main.c
+++ main.c
@@ -2,8 +2,27 @@
 #include <stdlib.h>
 #include <opencv/highgui.h>
 
+
+/*IplImage filtre(IplImage *img){
+	int i, j;
+	unsigned char* data = (unsigned char*)(img->imageData);
+	for (i = 0; i < img->height; i++){
+		for (j = 0; j < img->width;j++){
+			if (data[2+3*j+3*img->width*i] > 200){
+				data[0+3*j+3*img->width*i] = 0; //bleu
+				data[1+3*j+3*img->width*i] = 0; //vert
+				data[2+3*j+3*img->width*i] = 255; //rouge
+			}
+		}
+	}
+	return img;
+
+}*/
+
+
 int main (int argc, char* argv[]){
 	char key;
+	int i = 0, j = 0;
 	IplImage *image;
 	CvCapture *capture = cvCreateCameraCapture( CV_CAP_ANY );
 	if (!capture) {
@@ -13,6 +32,16 @@ int main (int argc, char* argv[]){
 
 		while(key != 'q' && key != 'Q') {
 			image  = cvQueryFrame(capture);
+			unsigned char* data = (unsigned char*)(image->imageData);
+			for (i = 0; i < image->height; i++){
+				for (j = 0; j < image->width;j++){
+					if (data[2+3*j+3*image->width*i] > 200 && data[0+3*j+3*image->width*i] < 200 && data[1+3*j+3*image->width*i] < 200){
+						data[0+3*j+3*image->width*i] = 0; //bleu
+						data[1+3*j+3*image->width*i] = 0; //vert
+						data[2+3*j+3*image->width*i] = 255; //rouge
+					}
+				}
+			}
 			cvShowImage("GeckoGeek Window", image);
 			key = cvWaitKey(10);
 		}
