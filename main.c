1c13eab18e0f6b86a48874b261db5b69b5979d4f
Timothee Bureaugodart <bureau_t@epita.fr>
2015-03-07 18:12:48 +0100

    détection contour

--- main.c
+++ main.c
@@ -22,7 +22,7 @@
 
 int main (int argc, char* argv[]){
 	char key;
-	int i = 0, j = 0;//, h = 0, w = 0;
+	int i = 0, j = 0, h = 0, w = 0;
 	IplImage *image;
 	CvCapture *capture = cvCreateCameraCapture( CV_CAP_ANY );  //capte image caméra
 	if (!capture) {
@@ -38,10 +38,50 @@ int main (int argc, char* argv[]){
 			unsigned char* data = (unsigned char*)(image->imageData);
 			for (i = 0; i < image->height; i++){   //set les pixels
 				for (j = 0; j < image->width;j++){
-					if (data[2+3*j+3*image->width*i] > 200 && data[0+3*j+3*image->width*i] < 200 && data[1+3*j+3*image->width*i] < 200){
+					if (data[2+3*j+3*image->width*i] > 200 && data[0+3*j+3*image->width*i] < 100 && data[1+3*j+3*image->width*i] < 100){
 						data[0+3*j+3*image->width*i] = 0; //bleu
 						data[1+3*j+3*image->width*i] = 0; //vert
 						data[2+3*j+3*image->width*i] = 255; //rouge
+						w = j - 40;
+						if (w > 0 && data[2+3*w+3*image->width*i] != 255 ){
+							for(h = i - 20; h < i + 20; h++){
+								if (h > 0 && h < image->height ){
+								data[0+3*w+3*image->width*h] = 0; 
+								data[1+3*w+3*image->width*h] = 255; 
+								data[2+3*w+3*image->width*h] = 0; 
+								}
+							}
+						}
+						w = j + 40;
+						if (w < image->width && data[2+3*w+3*image->width*i] != 255 ){
+							for(h = i - 20; h < i + 20; h++){
+								if (h > 0 && h < image->height ){
+								data[0+3*w+3*image->width*h] = 0;
+								data[1+3*w+3*image->width*h] = 255; 
+								data[2+3*w+3*image->width*h] = 0;
+								}
+							}
+						}
+						h = i - 40;
+						if (h > 0 && data[2+3*j+3*image->width*h] != 255 ){
+							for(w = j - 20; w < j + 20; w++){
+								if (w > 0 && w < image->height ){
+								data[0+3*w+3*image->width*h] = 0; 
+								data[1+3*w+3*image->width*h] = 255; 
+								data[2+3*w+3*image->width*h] = 0; 
+								}
+							}
+						}
+						h = i + 40;
+						if (h < image->height && data[2+3*j+3*image->width*h] != 255 ){
+							for(w = j - 20; w < j + 20; w++){
+								if (w > 0 && w < image->height ){
+								data[0+3*w+3*image->width*h] = 0; 
+								data[1+3*w+3*image->width*h] = 255; 
+								data[2+3*w+3*image->width*h] = 0;
+								}
+							}
+						}
 					}
 				}
 			}
@@ -53,7 +93,6 @@ int main (int argc, char* argv[]){
 		}
 		//free tout
 		cvDestroyAllWindows();
-		printf("%d %d", image->height, image->width);
 		cvReleaseCapture(&capture);
 		printf("Merci d'avoir utilisé notre logiciel de capture d'image.\n");
 	}
