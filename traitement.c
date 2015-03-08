9c18e3e092ad1f8afa1a16d082423460004314db
Sylvert Bernet <sylvert.bernet@epita.fr>
2015-03-08 22:57:59 +0100

    Repérage des trois points de couleur

--- traitement.c
+++ traitement.c
@@ -5,16 +5,56 @@
 #include "traitement.h"
 
 
-void set_color(IplImage *image, unsigned char* data){
+void set_color(char color, IplImage *image, unsigned char* data){
 	int i, j;
-	//unsigned char* data = (unsigned char*)(image->imageData);
-	//set les pixels
-	for (i = 0; i < image->height; i++){
-		for (j = 0; j < image->width;j++){
-			if (data[2+3*j+3*image->width*i] > 200 && data[0+3*j+3*image->width*i] < 130 && data[1+3*j+3*image->width*i] < 130){
-				data[0+3*j+3*image->width*i] = 0; //bleu
-				data[1+3*j+3*image->width*i] = 0; //vert
-				data[2+3*j+3*image->width*i] = 255; //rouge
+	if (color != 'a') {
+		int r=0,v=0,b=0;
+		int pix1, pix2, pix3;
+		if(color == 'r') {
+			r=255;
+			pix1 = 2;
+			pix2 = 0;
+			pix3 = 1;
+		}	else if (color == 'v'){
+			v=255;
+			pix1 = 1;
+			pix2 = 0;
+			pix3 = 2;
+		}	else { 
+			b=255;
+			pix1 = 0;
+			pix2 = 1;
+			pix3 = 2;
+		}
+		//unsigned char* data = (unsigned char*)(image->imageData);
+		//set les pixels
+		for (i = 0; i < image->height; i++){
+			for (j = 0; j < image->width;j++){
+				if (data[pix1+3*j+3*image->width*i] > 255 && data[pix2+3*j+3*image->width*i] < 130 && data[pix3+3*j+3*image->width*i] < 130){
+					data[0+3*j+3*image->width*i] = b; //bleu
+					data[1+3*j+3*image->width*i] = v; //vert
+					data[2+3*j+3*image->width*i] = r; //rouge
+				}
+			}
+		}
+	}else {
+		for (i = 0; i < image->height; i++){
+			for (j = 0; j < image->width;j++){
+				if (data[2+3*j+3*image->width*i] > 160 && data[0+3*j+3*image->width*i] < 100 && data[1+3*j+3*image->width*i] < 100){
+					data[0+3*j+3*image->width*i] = 0 ; //bleu
+					data[1+3*j+3*image->width*i] = 0; //vert
+					data[2+3*j+3*image->width*i] = 255; //rouge
+				}
+				if (data[2+3*j+3*image->width*i] < 100 && data[0+3*j+3*image->width*i] > 160 && data[1+3*j+3*image->width*i] < 100){
+					data[0+3*j+3*image->width*i] = 255 ; //bleu
+					data[1+3*j+3*image->width*i] = 0; //vert
+					data[2+3*j+3*image->width*i] = 0; //rouge
+				}
+				if (data[2+3*j+3*image->width*i] <60 && data[0+3*j+3*image->width*i] < 60 && data[1+3*j+3*image->width*i] > 80){
+					data[0+3*j+3*image->width*i] = 0 ; //bleu
+					data[1+3*j+3*image->width*i] = 255; //vert
+					data[2+3*j+3*image->width*i] = 0; //rouge
+				}
 			}
 		}
 	}
@@ -24,7 +64,7 @@ void set_color(IplImage *image, unsigned char* data){
 void filtre_carre(IplImage *img){
 	int i, j, h, w;
 	unsigned char* data = (unsigned char*)(img->imageData);
-	set_color(img, data);
+	set_color('a', img, data);
 	for (i = 0; i < img->height; i++){   //set les pixels
 		for (j = 0; j < img->width;j++){
 			if (data[2+3*j+3*img->width*i] > 200 && data[0+3*j+3*img->width*i] < 100 && data[1+3*j+3*img->width*i] < 100){
@@ -35,9 +75,9 @@ void filtre_carre(IplImage *img){
 				if (w > 0 && data[2+3*w+3*img->width*i] != 255 ){
 					for(h = i - 20; h < i + 20; h++){
 						if (h > 0 && h < img->height ){
-						data[0+3*w+3*img->width*h] = 0; 
-						data[1+3*w+3*img->width*h] = 255; 
-						data[2+3*w+3*img->width*h] = 0; 
+							data[0+3*w+3*img->width*h] = 0; 
+							data[1+3*w+3*img->width*h] = 255; 
+							data[2+3*w+3*img->width*h] = 0; 
 						}
 					}
 				}
@@ -45,9 +85,9 @@ void filtre_carre(IplImage *img){
 				if (w < img->width && data[2+3*w+3*img->width*i] != 255 ){
 					for(h = i - 20; h < i + 20; h++){
 						if (h > 0 && h < img->height ){
-						data[0+3*w+3*img->width*h] = 0;
-						data[1+3*w+3*img->width*h] = 255; 
-						data[2+3*w+3*img->width*h] = 0;
+							data[0+3*w+3*img->width*h] = 0;
+							data[1+3*w+3*img->width*h] = 255; 
+							data[2+3*w+3*img->width*h] = 0;
 						}
 					}
 				}
@@ -55,9 +95,9 @@ void filtre_carre(IplImage *img){
 				if (h > 0 && data[2+3*j+3*img->width*h] != 255 ){
 					for(w = j - 20; w < j + 20; w++){
 						if (w > 0 && w < img->height ){
-						data[0+3*w+3*img->width*h] = 0; 
-						data[1+3*w+3*img->width*h] = 255; 
-						data[2+3*w+3*img->width*h] = 0; 
+							data[0+3*w+3*img->width*h] = 0; 
+							data[1+3*w+3*img->width*h] = 255; 
+							data[2+3*w+3*img->width*h] = 0; 
 						}
 					}
 				}
@@ -65,9 +105,9 @@ void filtre_carre(IplImage *img){
 				if (h < img->height && data[2+3*j+3*img->width*h] != 255 ){
 					for(w = j - 20; w < j + 20; w++){
 						if (w > 0 && w < img->height ){
-						data[0+3*w+3*img->width*h] = 0; 
-						data[1+3*w+3*img->width*h] = 255; 
-						data[2+3*w+3*img->width*h] = 0;
+							data[0+3*w+3*img->width*h] = 0; 
+							data[1+3*w+3*img->width*h] = 255; 
+							data[2+3*w+3*img->width*h] = 0;
 						}
 					}
 				}
@@ -78,17 +118,10 @@ void filtre_carre(IplImage *img){
 void filtre_forme(IplImage *image){
 	int i = 0, j = 0, h = 0, w = 0, y = 0, z = 0;
 	bool tour;
+	int moy_xr=0, moy_yr=0, moy_xv=0, moy_yv=0, moy_xb=0,moy_yb=0;
+	int nb_r=0,nb_v=0,nb_b=0;
 	unsigned char* data = (unsigned char*)(image->imageData);
-	//set les pixels
-	for (i = 0; i < image->height; i++){
-		for (j = 0; j < image->width;j++){
-			if (data[2+3*j+3*image->width*i] > 200 && data[0+3*j+3*image->width*i] < 130 && data[1+3*j+3*image->width*i] < 130){
-				data[0+3*j+3*image->width*i] = 0; //bleu
-				data[1+3*j+3*image->width*i] = 0; //vert
-				data[2+3*j+3*image->width*i] = 255; //rouge
-			}
-		}
-	}
+	set_color('a', image, data);				
 	//set les carré
 	for (i = 0; i < image->height; i++){
 		for (j = 0; j < image->width; j++){
@@ -112,8 +145,72 @@ void filtre_forme(IplImage *image){
 							if(h > 0 && h < image->height && w > 0 && w < image->width && data[2+3*w+3*image->width*h] != 255){
 								if((h == i-10 || h == i+10) || (w == i+10 || w == j+10)){
 									data[0+3*w+3*image->width*h] = 0; 
-									data[1+3*w+3*image->width*h] = 255; 
-									data[2+3*w+3*image->width*h] = 0;
+									data[1+3*w+3*image->width*h] = 240; 
+									data[2+3*w+3*image->width*h] = 240;
+									moy_xr +=w;
+									moy_yr +=h;
+									nb_r++;
+								}
+							}
+						}
+					}
+				}
+			}
+			//Pixel bleu ?
+			if (data[0+3*j+3*image->width*i] == 255){	
+				tour = true;
+				for(y = i-5; y <= i+5; y++){
+					for(z = j+5; z <= j+5; z++){
+						if(y > 0 && y < image->height && z > 0 && z < image->width){
+							if(data[0+3*z+3*image->width*y] != 255 || data[2+3*z+3*image->width*y] != 0 || data[1+3*z+3*image->width*y] != 0)
+								tour = false;
+							if(!tour) break;
+						}
+						if(!tour) break;
+					}
+				}
+				if(tour){
+					for(h = i-10; h <= i+10; h++){
+						for( w= j-10; w <= j+10; w++){
+							if(h > 0 && h < image->height && w > 0 && w < image->width && data[0+3*w+3*image->width*h] != 255){
+								if((h == i-10 || h == i+10) || (w == i+10 || w == j+10)){
+									data[0+3*w+3*image->width*h] = 0; 
+									data[1+3*w+3*image->width*h] = 240; 
+									data[2+3*w+3*image->width*h] = 240;
+									moy_xb +=w;
+									moy_yb +=h;
+									nb_b++;
+		
+								}
+							}
+						}
+					}
+				}
+			}//Pixel vert ?
+			if (data[1+3*j+3*image->width*i] == 255){	
+				tour = true;
+				for(y = i-5; y <= i+5; y++){
+					for(z = j+5; z <= j+5; z++){
+						if(y > 0 && y < image->height && z > 0 && z < image->width){
+							if(data[1+3*z+3*image->width*y] != 255 || data[0+3*z+3*image->width*y] != 0 || data[2+3*z+3*image->width*y] != 0)
+								tour = false;
+							if(!tour) break;
+						}
+						if(!tour) break;
+					}
+				}
+				if(tour){
+					for(h = i-10; h <= i+10; h++){
+						for( w= j-10; w <= j+10; w++){
+							if(h > 0 && h < image->height && w > 0 && w < image->width && data[1+3*w+3*image->width*h] != 255){
+								if((h == i-10 || h == i+10) || (w == i+10 || w == j+10)){
+									data[0+3*w+3*image->width*h] = 0; 
+									data[1+3*w+3*image->width*h] = 240; 
+									data[2+3*w+3*image->width*h] = 240;
+									moy_xv +=w;
+									moy_yv +=h;
+									nb_v++;
+		
 								}
 							}
 						}
@@ -121,6 +218,25 @@ void filtre_forme(IplImage *image){
 				}
 			}
 		}
+	}
+	for (int i = -5; i < 6; i++) {
+	for (int j = -5; j < 6; j++) {
+		if (nb_b > 0) {  
+		data[0+3*(i+(moy_xb/nb_b))+3*image->width*(j+(moy_yb/nb_b))]= 240;
+		data[1+3*(i+(moy_xb/nb_b))+3*image->width*(j+(moy_yb/nb_b))]= 0;
+		data[2+3*(i+(moy_xb/nb_b))+3*image->width*(j+(moy_yb/nb_b))]= 240;
+	}
+	if (nb_v > 0) {  
+		data[0+3*(i+(moy_xv/nb_v))+3*image->width*(j+(moy_yv/nb_v))]= 240;
+		data[1+3*(i+(moy_xv/nb_v))+3*image->width*(j+(moy_yv/nb_v))]= 0;
+		data[2+3*(i+(moy_xv/nb_v))+3*image->width*(j+(moy_yv/nb_v))]= 240;
+	}
+	if (nb_r > 0) {  
+		data[0+3*(i+(moy_xr/nb_r))+3*image->width*(j+(moy_yr/nb_r))]= 240;
+		data[1+3*(i+(moy_xr/nb_r))+3*image->width*(j+(moy_yr/nb_r))]= 0;
+		data[2+3*(i+(moy_xr/nb_r))+3*image->width*(j+(moy_yr/nb_r))]= 240;
+	}
+	}
 	}
 }
 
@@ -131,7 +247,7 @@ int traitement(){
 	IplImage *image;
 	CvCapture *capture = cvCreateCameraCapture( CV_CAP_ANY );  //capte image caméra
 	if (!capture) {
-   		printf("Ouverture du flux vidéo impossible !\n");
+		printf("Ouverture du flux vidéo impossible !\n");
 	}else{
 		cvNamedWindow("GeckoGeek Window", CV_WINDOW_AUTOSIZE); //fait la fenêtre
 
