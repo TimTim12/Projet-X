5a41dc098ceea9613d196cda30fc12fe13879c46
Timothee Bureaugodart <bureau_t@epita.fr>
2015-03-08 15:43:09 +0100

    modif de compile

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 #include <stdbool.h>
 
 
-/*int* filtre(IplImage *im){
+void filtre_carre(IplImage *img){
 	int i, j, h, w;
 	unsigned char* data = (unsigned char*)(img->imageData);
 	for (i = 0; i < img->height; i++){   //set les pixels
@@ -57,14 +57,59 @@
 		}
 	}
 }
+void filtre_forme(IplImage *image){
+	int i = 0, j = 0, h = 0, w = 0, y = 0, z = 0;
+	bool tour;
+	unsigned char* data = (unsigned char*)(image->imageData);
+	//set les pixels
+	for (i = 0; i < image->height; i++){
+		for (j = 0; j < image->width;j++){
+			if (data[2+3*j+3*image->width*i] > 200 && data[0+3*j+3*image->width*i] < 130 && data[1+3*j+3*image->width*i] < 130){
+				data[0+3*j+3*image->width*i] = 0; //bleu
+				data[1+3*j+3*image->width*i] = 0; //vert
+				data[2+3*j+3*image->width*i] = 255; //rouge
+			}
+		}
+	}
+	//set les carré
+	for (i = 0; i < image->height; i++){
+		for (j = 0; j < image->width; j++){
+			tour = false;
+			//regarde si les pixel autour sont rouge
+			if (data[2+3*j+3*image->width*i] == 255){	
+				tour = true;
+				for(y = i-5; y <= i+5; y++){
+					for(z = j+5; z <= j+5; z++){
+						if(y > 0 && y < image->height && z > 0 && z < image->width){
+							if(data[2+3*z+3*image->width*y] != 255 || data[0+3*z+3*image->width*y] != 0 || data[1+3*z+3*image->width*y] != 0)
+								tour = false;
+							if(!tour) break;
+						}
+						if(!tour) break;
+					}
+				}
+				if(tour){
+					for(h = i-10; h <= i+10; h++){
+						for( w= j-10; w <= j+10; w++){
+							if(h > 0 && h < image->height && w > 0 && w < image->width && data[2+3*w+3*image->width*h] != 255){
+								if((h == i-10 || h == i+10) || (w == i+10 || w == j+10)){
+									data[0+3*w+3*image->width*h] = 0; 
+									data[1+3*w+3*image->width*h] = 255; 
+									data[2+3*w+3*image->width*h] = 0;
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
 
 
-*/
 
 int main (int argc, char* argv[]){
 	char key;
-	int i = 0, j = 0, h = 0, w = 0, y = 0, z = 0;
-	bool tour = true;
 	IplImage *image;
 	CvCapture *capture = cvCreateCameraCapture( CV_CAP_ANY );  //capte image caméra
 	if (!capture) {
@@ -74,52 +119,8 @@ int main (int argc, char* argv[]){
 
 		while(key != 'q' && key != 'Q') {
 			image  = cvQueryFrame(capture);
-
-		//fonction à mettre ailleurs que dans main
-		//{
-			unsigned char* data = (unsigned char*)(image->imageData);
-			for (i = 0; i < image->height; i++){   //set les pixels
-				for (j = 0; j < image->width;j++){
-					if (data[2+3*j+3*image->width*i] > 200 && data[0+3*j+3*image->width*i] < 130 && data[1+3*j+3*image->width*i] < 130){
-						data[0+3*j+3*image->width*i] = 0; //bleu
-						data[1+3*j+3*image->width*i] = 0; //vert
-						data[2+3*j+3*image->width*i] = 255; //rouge
-					}
-				}
-			}
-			for (i = 0; i < image->height; i++){   //set les carré
-				for (j = 0; j < image->width; j++){
-					tour = false;
-					if (data[2+3*j+3*image->width*i] == 255){		//regarde si les pixel autour sont rouge
-						tour = true;
-						for(y = i-5; y <= i+5; y++){
-							for(z = j+5; z <= j+5; z++){
-								if(y > 0 && y < image->height && z > 0 && z < image->width){
-									if(data[2+3*z+3*image->width*y] != 255 || data[0+3*z+3*image->width*y] != 0 || data[1+3*z+3*image->width*y] != 0)
-										tour = false;
-									if(!tour) break;
-								}
-								if(!tour) break;
-							}
-						}
-						if(tour){
-							for(h = i-10; h <= i+10; h++){
-								for( w= j+10; w <= j+10; w++){
-									if(h > 0 && h < image->height && w > 0 && w < image->width && data[2+3*w+3*image->width*h] != 255){
-										if((h == 1 || h == i+10) || (w == 1 || w == j+10)){
-											data[0+3*w+3*image->width*h] = 0; 
-											data[1+3*w+3*image->width*h] = 255; 
-											data[2+3*w+3*image->width*h] = 0;
-										}
-									}
-								}
-							}
-						}
-					}
-				}
-			}
-		//}
-
+			//filtre_carre(image);
+			filtre_forme(image);
 			//affiche et attend entré clavier pendant 10ms
 			cvShowImage("GeckoGeek Window", image);
 			key = cvWaitKey(10);
