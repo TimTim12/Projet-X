dc53f1027a415a7b67b39612e0564e6eac425abc
Bob-Lerouge <robin.duprat@epita.fr>
2015-03-08 17:30:19 +0100

    MAJ Makefile (header) + creation traitement.h & projetX.c

---
+++ traitement.h
@@ -0,0 +1,9 @@
+#ifndef _TRAITEMENT_H
+#define _TRAITEMENT_H
+
+void set_color(IplImage *image, unsigned char* data);
+void filtre_carre(IplImage *img);
+void filtre_forme(IplImage *image);
+int traitement();
+
+#endif 
