a1e35cabdda247634cb6c4eaefa7070699f5f6dc
Timtim <bureau_t@epita.fr>
2015-04-06 01:12:00 +0200

    fix some stuf

--- traitement.c
+++ traitement.c
@@ -3,22 +3,20 @@
 #include <opencv/cv.h>
 #include <opencv/highgui.h>
 #include <iostream>
-#include <stdbool.h>
 #include "traitement.h"
 
-// Maths methods
+
 #define max(a, b) ((a) > (b) ? (a) : (b))
 #define min(a, b) ((a) < (b) ? (a) : (b))  
 #define abs(x) ((x) > 0 ? (x) : -(x))
 #define sign(x) ((x) > 0 ? 1 : -1)
 
-// Step mooving for object min & max
+
 #define STEP_MIN 5
 #define STEP_MAX 100 
 
-// Position of the object we overlay
+// Position de l'objet, "couleur", seuil de tolérance
 CvPoint objectPos = cvPoint(-1, -1);
-// Color tracked and our tolerance towards it
 int h = 0, s = 0, v = 0, tolerance = 10;
 
 IplImage *image;
@@ -56,28 +54,24 @@ CvPoint binarisation(IplImage* image, int *nbPixels) {
 	int sommeX = 0, sommeY = 0;
 	*nbPixels = 0;
 
-	// Create the mask &initialize it to white (no color detected)
+	// création du masque blanc
 	mask = cvCreateImage(cvGetSize(image), image->depth, 1);
 
-	// Create the hsv image
+	// image (Hue, Saturation, Value, en français : TSV – Teinte, Saturation, Valeur)
 	hsv = cvCloneImage(image);
 	cvCvtColor(image, hsv, CV_BGR2HSV);
-
-	// We create the mask
+	
+	//mets à jour le masque
 	cvInRangeS(hsv, cvScalar(h - tolerance -1, s - tolerance, 0), cvScalar(h + tolerance -1, s + tolerance, 255), mask);
 
-	// Create kernels for the morphological operation
+	//élimination des pixel isolé 
 	kernel = cvCreateStructuringElementEx(5, 5, 2, 2, CV_SHAPE_ELLIPSE);
-
-	// Morphological opening (inverse because we have white pixels on black background)
 	cvDilate(mask, mask, kernel, 1);
 	cvErode(mask, mask, kernel, 1);  
 
-	// We go through the mask to look for the tracked object and get its gravity center
+	// calcule du centre de l'objet que l'on veut
 	for(x = 0; x < mask->width; x++) {
-		for(y = 0; y < mask->height; y++) { 
-
-			// If its a tracked pixel, count it to the center of gravity's calcul
+		for(y = 0; y < mask->height; y++) {
 			if(((uchar *)(mask->imageData + y*mask->widthStep))[x] == 255) {
 				sommeX += x;
 				sommeY += y;
@@ -85,19 +79,16 @@ CvPoint binarisation(IplImage* image, int *nbPixels) {
 			}
 		}
 	}
-
-	// Show the result of the mask image
+	
+	
 	cvShowImage("GeckoGeek Mask", mask);
 
-	// We release the memory of kernels
+	//free tout 
 	cvReleaseStructuringElement(&kernel);
-
-	// We release the memory of the mask
 	cvReleaseImage(&mask);
-	// We release the memory of the hsv image
 	cvReleaseImage(&hsv);
 
-	// If there is no pixel, we return a center outside the image, else we return the center of gravity
+	//si pas d'objet dans la vidéo, point hors champ 
 	if(*nbPixels > 0)
 		return cvPoint((int)(sommeX / (*nbPixels)), (int)(sommeY / (*nbPixels)));
 	else
@@ -105,23 +96,22 @@ CvPoint binarisation(IplImage* image, int *nbPixels) {
 }
 
 
-/*
- * Add a circle on the video that fellow your colored object
- */
+
+
+
 void addObjectToVideo(IplImage* image, CvPoint objectNextPos, int nbPixels) {
 
 	int objectNextStepX, objectNextStepY;
 
-	//	Calculate circle next position (if there is enough pixels)
-	if (nbPixels > 10) {
+		if (nbPixels > 10) {
 
-		// Reset position if no pixel were found
+		// Reset position 
 		if (objectPos.x == -1 || objectPos.y == -1) {
 			objectPos.x = objectNextPos.x;
 			objectPos.y = objectNextPos.y;
 		}
 
-		// Move step by step the object position to the desired position
+		//lissage du déplacement 
 		if (abs(objectPos.x - objectNextPos.x) > STEP_MIN) {
 			objectNextStepX = max(STEP_MIN, min(STEP_MAX, abs(objectPos.x - objectNextPos.x) / 2));
 			objectPos.x += (-1) * sign(objectPos.x - objectNextPos.x) * objectNextStepX;
@@ -131,17 +121,16 @@ void addObjectToVideo(IplImage* image, CvPoint objectNextPos, int nbPixels) {
 			objectPos.y += (-1) * sign(objectPos.y - objectNextPos.y) * objectNextStepY;
 		}
 
-		// -1 = object isn't within the camera range
+		// -1 = objet hors caméra
 	} else {
 
 		objectPos.x = -1;
 		objectPos.y = -1;
-
 	}
 
-	// Draw an object (circle) centered on the calculated center of gravity
+	//Dessine moi un mouton	
 	if (nbPixels > 10)
-		cvDrawCircle(image, objectPos, 15, CV_RGB(255, 0, 0), -1);
+		cvDrawCircle(image, objectPos, 5, CV_RGB(255, 0, 0), -1);
 
 	cvShowImage("GeckoGeek Color Tracking", image);
 }
@@ -149,27 +138,21 @@ void addObjectToVideo(IplImage* image, CvPoint objectNextPos, int nbPixels) {
 
 void getObjectColor(int event, int x, int y, int flags, void *param = NULL) {
 
-	// Vars
 	CvScalar pixel;
 	IplImage *hsv;
 
 	if(event == CV_EVENT_LBUTTONUP) {
 
-		// Get the hsv image
 		hsv = cvCloneImage(image);
 		cvCvtColor(image, hsv, CV_BGR2HSV);
-
-		// Get the selected pixel
 		pixel = cvGet2D(hsv, y, x);
 
-		// Change the value of the tracked color with the color of the selected pixel
+		// Mets à jour la couleur rechercher
 		h = (int)pixel.val[0];
 		s = (int)pixel.val[1];
 		v = (int)pixel.val[2];
 
-		// Release the memory of the hsv image
 		cvReleaseImage(&hsv);
-
 	}
 
 }
@@ -190,7 +173,7 @@ int traitement(){
 		cvMoveWindow("GeckoGeek Color Tracking", 0, 100);
 		cvMoveWindow("GeckoGeek Mask", 650, 100);
 
-		// Mouse event to select the tracked color on the original image
+		// clique souris
 		cvSetMouseCallback("GeckoGeek Color Tracking", getObjectColor);
 
 		while(key != 'q' && key != 'Q') {
